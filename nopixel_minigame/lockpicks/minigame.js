582fba67542e0aaa472bb2f6f685a8f8009a421f
Sharkiller <sharkiller@users.noreply.github.com>
2021-12-11 15:24:05 -0300

    Updated lockpicks

--- nopixel_minigame/lockpicks/minigame.js
+++ nopixel_minigame/lockpicks/minigame.js
@@ -6,9 +6,11 @@ let H = canvas.height;
 let degrees = 0;
 let new_degrees = 0;
 let time = 0;
-let color = "#ffffff";
+let color = "#ff0000";
+let txtcolor = "#ffffff";
 let bgcolor = "#404b58";
 let bgcolor2 = "#41a491";
+let bgcolor3 = "#00ff00";
 let key_to_press;
 let g_start, g_end;
 let animation_loop;
@@ -22,7 +24,7 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
 }
 
-function init() {
+function init(correct) {
     // Clear the canvas every time a chart is drawn
     ctx.clearRect(0,0,W,H);
 
@@ -35,7 +37,7 @@ function init() {
 
     // Green zone
     ctx.beginPath();
-    ctx.strokeStyle = bgcolor2;
+    ctx.strokeStyle = correct === true? bgcolor3 : bgcolor2;
     ctx.lineWidth = 20;
     ctx.arc(W / 2, H / 2, 100, g_start - 90 * Math.PI / 180, g_end - 90 * Math.PI / 180, false);
     ctx.stroke();
@@ -44,12 +46,12 @@ function init() {
     let radians = degrees * Math.PI / 180;
     ctx.beginPath();
     ctx.strokeStyle = color;
-    ctx.lineWidth = 20;
-    ctx.arc(W / 2, H / 2, 100, 0 - 90 * Math.PI / 180, radians - 90 * Math.PI / 180, false);
+    ctx.lineWidth = 40;
+    ctx.arc(W / 2, H / 2, 90, radians - 0.1 - 90 * Math.PI / 180, radians - 90 * Math.PI / 180, false);
     ctx.stroke();
 
     // Adding the key_to_press
-    ctx.fillStyle = color;
+    ctx.fillStyle = txtcolor;
     ctx.font = "100px sans-serif";
     let text_width = ctx.measureText(key_to_press).width;
     ctx.fillText(key_to_press, W / 2 - text_width / 2, H / 2 + 35);
@@ -62,7 +64,7 @@ function draw() {
     document.querySelector('.stats .max-streak').innerHTML = max_streak;
 
     g_start = getRandomInt(20,40) / 10;
-    g_end = getRandomInt(5,10) / 10;
+    g_end = getRandomInt(3,5) / 10;
     g_end = g_start + g_end;
 
     degrees = 0;
@@ -70,15 +72,17 @@ function draw() {
 
     key_to_press = ''+getRandomInt(1,4);
 
-    time = getRandomInt(1, 3) * 5;
+    time = getRandomInt(1, 3) * 6;
 
     animation_loop = setInterval(animate_to, time);
 }
 
 function animate_to() {
     if (degrees >= new_degrees) {
+        document.querySelector('.text.status').innerHTML = 'Skipped!';
         console.log('Failed: timeout!');
         wrong();
+        draw();
         return;
     }
 
@@ -92,7 +96,6 @@ function correct(){
         max_streak = streak;
     }
     streak++;
-    draw();
 }
 
 function wrong(){
@@ -101,7 +104,6 @@ function wrong(){
         max_streak = streak;
     }
     streak = 0;
-    draw();
 }
 
 document.addEventListener("keydown", function(ev) {
@@ -109,23 +111,31 @@ document.addEventListener("keydown", function(ev) {
     let valid_keys = ['1','2','3','4'];
 
     if( valid_keys.includes(key_pressed) ){
+        bgcolor3 = "#ff0000";
         if( key_pressed === key_to_press ){
             let d_start = (180 / Math.PI) * g_start;
             let d_end = (180 / Math.PI) * g_end;
             if( degrees < d_start ){
-                console.log('Failed: Too soon!');
+                document.querySelector('.text.status').innerHTML = 'Too soon!';
                 wrong();
             }else if( degrees > d_end ){
-                console.log('Failed: Too late!');
+                document.querySelector('.text.status').innerHTML = 'Too late!';
                 wrong();
             }else{
-                console.log('Success!');
+                document.querySelector('.text.status').innerHTML = 'Success!';
+                bgcolor3 = "#00ff00";
                 correct();
             }
         }else{
+            document.querySelector('.text.status').innerHTML = 'Failed: Pressed '+key_pressed;
             console.log('Failed: Pressed '+key_pressed+' instead of '+key_to_press);
             wrong();
         }
+        init(true);
+        clearInterval(animation_loop);
+        setTimeout(function(){
+            draw();
+        }, 500);
     }
 });
 
